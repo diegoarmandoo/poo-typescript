6adcfda3af9d45dd9dc5e5c06a759417ae1771af
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-05-17 22:46:22 -0300

    ♻️refactor(Index): Exemplifica o Uso do Método Estático | Parte 03
    - Instancia um novo objeto Pessoa;
    - Imprime no console os dados dos objetos criados e a quantidade de objetos Pessoa instanciados.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { Pessoa } from "./pessoa";
 
 const diego: Pessoa = new Pessoa("Diego", "Armando de Oliveira ", 36, true, ['Rua A','Rua C']);
 const maria: Pessoa = new Pessoa("Maria", "José", 25, false, ['Rua B']);
+const pedro: Pessoa = new Pessoa("Pedro", "José", 35, true, ['Rua C']);
 
 console.log(diego.exibirNomeCompleto());
 console.log(diego.recuperarEndereco(0));
+console.log(diego.nome);
 console.log(diego);
 console.log("-------------------------------")
 console.log(maria.exibirNomeCompleto());
 console.log(maria.recuperarEndereco(0));
-console.log(maria);
+console.log(maria.nome);
+console.log("-------------------------------")
+console.log(Pessoa.getQuantidadePessoas());

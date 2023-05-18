db89bfd58bd7ea4e6d9432f9d9e540ce0ff939d0
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-05-18 03:28:35 -0300

    ♻️refactor(Index): Testa os Modificadores de Acesso | Parte 06
    - Instancia um objetos de Endereco;
    - Instancia um objeto Aluno e um Professor;
    - Demonstra o funcionamento dos modificadores de acesso e os Gets e Sets.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import { Pessoa } from "./pessoa";
-import { Aluno, SituacaoAluno } from "./aluno";
-import { Professor } from "./professor";
-import { Endereco } from "./endereco";
+import { Pessoa } from "./pessoa/pessoa";
+import { Aluno, SituacaoAluno } from "./aluno/aluno";
+import { Professor } from "./professor/professor";
+import { Endereco } from "./endereco/endereco";
 
 const endereco01: Endereco = new Endereco("490000",12,"Rua A","Bairro A","Cidade A","Estado A");
 const endereco02: Endereco = new Endereco("49888",63,"Rua b","Bairro B","Cidade B","Estado B");
 
-const felipe: Aluno = new Aluno("Felipe","Jose","Masculino","9999",34,true,[endereco01],"Informatica",SituacaoAluno.CONCLUIDO); 
-const luis: Professor = new Professor("Luis","Santos","Masculino","9999",56,false,[endereco01,endereco02],1000);
+const felipe: Aluno = new Aluno("Felipe","Jose","Masculino","999.999.999-99",34,true,[endereco01],"Informatica",SituacaoAluno.CONCLUIDO); 
+const luis: Professor = new Professor("Luis","Santos","Masculino","929.939.199-29",56,false,[endereco01,endereco02],1000);
 
-felipe.submeterProjetoPesquisa();
-felipe.submeterProjetoExtensao();
-luis.submeterProjetoPesquisa();
-luis.submeterProjetoExtensao();
+console.log(felipe);
+console.log(luis);
 
 console.log("-------------------------------")
 console.log(Pessoa.getQuantidadePessoas());

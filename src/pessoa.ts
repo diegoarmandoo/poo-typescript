165160562f5935265d9355d1d87d025e92c39667
Diego Armando O. Meneses <diegoarmandoo@hotmail.com>
2023-05-18 00:34:22 -0300

    ✨feat(Pessoa): Aprimora o Fundamento da Abstração | Parte 04
    - Define a classe como abstrata;
    - Implementa os atributos e métodos duplicados em Aluno e Professor (aplicando fundamento do reuso);
    - Refatora o construtor para receber novos parâmetros e atribuir valores aos novos atributos;
    - Define um método abstrato (assinatura) para moldar o comportamento de gerar matrícula para as classes que herdarem.

--- src/pessoa.ts
+++ src/pessoa.ts
@@ -1,21 +1,42 @@
-class Pessoa {
+abstract class Pessoa {
 
-    nome: string;
-    sobrenome: string;
-    idade: number;
-    brasileiro: boolean;
-    enderecos: string[]; // Ou let enderecos: Array<string>;
+    //////////////////////////
+	//Atributos de Instância//
+	//////////////////////////
+
+    matricula: number = 0;
+    nome: string = '';
+    sobrenome: string = '';
+    sexo: string = '';
+    CPF: string = '';
+    idade: number = 0;
+    brasileiro: boolean = true;
+    enderecos: string[] = []; // Ou let enderecos: Array<string>;
+
+    ///////////////////////
+	//Atributos Estáticos//
+	///////////////////////
 
     static quantidadePessoas: number = 0;
 
-    constructor(nome: string, sobrenome: string, idade: number, brasileiro: boolean, enderecos: string[]){
+    //////////////
+	//Construtor//
+	//////////////
+
+    constructor(nome: string, sobrenome: string, sexo: string, CPF: string, idade: number, brasileiro: boolean, enderecos: string[]){
         this.nome = nome;
         this.sobrenome = sobrenome;
+        this.sexo = sexo;
+        this.CPF = CPF;
 		this.idade = idade;
 		this.brasileiro = brasileiro;
 		this.enderecos = enderecos;
         Pessoa.quantidadePessoas++;
     }
+
+    ////////////////////////
+	//Métodos de Instância//
+	////////////////////////
 
     exibirNomeCompleto(): string  {
         return (`O nome completo é: ${this.nome} ${this.sobrenome}`);
@@ -25,9 +46,22 @@ class Pessoa {
         return this.enderecos[posicao];
     }
 
+    /////////////////////
+	//Métodos Estáticos//
+	/////////////////////
+
     static getQuantidadePessoas(): number {
         return Pessoa.quantidadePessoas;
     }
+
+    /////////////////////
+	//Métodos Abstratos//
+	/////////////////////
+
+    //Método abstrato - Sem implementação
+	//Usado para garantir que as classes que herdem de pessoa possuam uma implementação do gerarMatricula, 
+	//assim é possivel que cada classe que herde de pessoa implemente sua forma específica de gerar matricula (Polimorfismo)
+    abstract gerarMatricula(): number;
 
 }
 
